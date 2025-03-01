6584e4774a47b87aa9304cff105e121dd35a1494
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2025-03-01 18:01:55 +0900

    Delete lang_app directory

--- lang_app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from .routes import main
-
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = "16"  # 세션에 필요한 secret key 설정
-    app.register_blueprint(main, url_prefix="/")  # Blueprint 등록
-    return app
-
